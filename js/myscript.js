4dce20b7a4ca46f744eab072b3de2deab1cba33e
sofia-papa <papa.sofia@outlook.it>
2021-09-03 15:06:58 +0100

    alert + countdown

---
+++ js/myscript.js
@@ -0,0 +1,41 @@
+
+//tramite un alert vengono mostrati cinque numeri da 1 a 100 generati casualmente
+for(let i = 0; i < 5; i++){
+    alert(getRandomInt(1,100));
+}
+
+// TIMER DI 30 SECONDI
+let tempoRimasto = 30;
+let elem = document.getElementById("timer");
+let timerId = setInterval(countdown, 1000);
+
+function countdown() {
+    if (tempoRimasto == -1) {
+        clearTimeout(timerId);
+        inseriscoNumeri();
+    } else {
+        elem.innerHTML = tempoRimasto + " SECONDI RIMASTI";
+        tempoRimasto--;
+    }
+}
+
+function inseriscoNumeri() { //al termine dei trenta secondi chiedo all'utente di inserire
+                             // i cinque numeri che ha visto precedentemente
+    for(let j = 0; j < 5; j++){
+        prompt("inserisci i numeri che hai appena visto");
+    }
+}
+
+
+
+
+
+
+
+
+//funzione per ottenere un numero randomico 
+function getRandomInt(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
