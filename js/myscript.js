e08bdcbd8441425c54e79ff49a9e7120c2bb2b66
sofia-papa <papa.sofia@outlook.it>
2021-09-03 17:12:41 +0100

    corretto fuction

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,6 @@
 
 let numeriGenerati = [];
+let numeriUtente =  [] ;
 //tramite un alert vengono mostrati cinque numeri da 1 a 100 generati casualmente
 for(let i = 0; i < 5; i++){
     numeriGenerati.push(getRandomInt(1,100));
@@ -8,34 +9,31 @@ for(let i = 0; i < 5; i++){
 console.log(numeriGenerati);
 
 // TIMER DI 30 SECONDI
-let tempoRimasto = 30;
+let tempoRimasto = 5;
 let elem = document.getElementById("timer");
 let timerId = setInterval(countdown, 1000);
 
 function countdown() {
     if (tempoRimasto == -1) {
-        clearTimeout(timerId);
-        inseriscoNumeri();
+        clearInterval(timerId);
+        numeri = inseriscoEVerificoNumeri();
+        console.log(numeri);
     } else {
         elem.innerHTML = tempoRimasto + " SECONDI RIMASTI";
         tempoRimasto--;
     }
 }
 
-function inseriscoNumeri() { //al termine dei trenta secondi chiedo all'utente di inserire
-                             // i cinque numeri che ha visto precedentemente
+function inseriscoEVerificoNumeri() { //al termine dei trenta secondi chiedo all'utente di inserire
+    let numeri = [];                  // i cinque numeri che ha visto precedentemente
     for(let j = 0; j < 5; j++){
-        prompt("inserisci i numeri che hai appena visto");
-
+      let num = prompt("inserisci i numeri che hai appena visto");
+        if (!numeri.includes(num)){ 
+            numeri.push(num);
+        } 
     }
+    return numeri;
 }
-
-
-
-
-
-
-
 
 //funzione per ottenere un numero randomico 
 function getRandomInt(min,max){
